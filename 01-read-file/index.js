fa8b7c2762102791533876c65bed209ec08a3298
Alexey <alexledovit@gmail.com>
2023-05-08 23:13:58 +0300

    fix: refactor code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,15 @@
-const { ReadStream } = require('./text.txt');
+
 const fs = require('fs');
+const path = require('path');
 
-
-//let file = ReadStream;
-fs.ReadStream('text.txt', "utf-8");
-
-
+    fs.readFile(
+        path.join(__dirname, './text.txt',),
+        'utf-8',
+        (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        }
+    );
   
 
 
