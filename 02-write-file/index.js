fb30772104af7b0049f7652540f3c0ae4fad6ba2
Alexey <alexledovit@gmail.com>
2023-05-09 01:50:22 +0300

    feat: при запуске будет создаваться текст new_text.txt в корневой папке
    При запуске кода создается текстовый файл new_text.txt и в него записывается сообщение из let messege.
    В консоли при запуске node index, код запросит написать текст и потом его напишет наоборот и закончит работу. К сожалению не удалось всё сделать из требований к заданию... Увы

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,35 @@
 
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const process = require('process');
+const { stdin, stdout } = process;
 
 
 
+let messege = "Сообщение создает файл";
 
-let messege = 'Привет ! Если ты это видишь';
-    fs.writeFile('./02-write-file/new_text.txt', messege, (err) => {
-        if(err) throw err;
-        console.log('Привет! Напише, что-то и увидешь чудо');
+fs.writeFile('./02-write-file/new_text.txt',messege , "utf-8",(data, err) => {
+    if(err) throw err;
+    console.log("Сообщение создало файл");
+});
+
+/*fs.readFile('/02-write-file/new_text.txt', 'utf-8');
+
+    stdout.write(`\n${messege}`);
+    stdin.on('data', data => {
+        
+        const name = data.toString();
+        const reverseName = name.split('').reverse().join('');
+        stdout.write(`\nМы всё это зашифровали ${reverseName}`);
+        process.exit();
     });
+
+   const { stdin, stdout } = process;*/
+stdout.write('Напиши сообщение и мы его зашифруем\n')
+stdin.on('data', data => {
+  const text = data.toString();
+  const reverseText = text.split('').reverse().join('');
+
+  stdout.write(`\nВаше сообщение зашифровано ${reverseText}`);
+  process.exit();
+});
